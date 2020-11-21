0a5ee4471b43478cb901734f581992af3e42ab1e
araamz <araamzaremehrjardi@gmail.com>
2020-11-21 01:07:08 -0800

    Updated main.ino -> Early Error State functionality
    -> Added Idle State to Error State transition.
    -> Added ADC Interrupt for continuous water level checking.
    -> Renamed several variables to undecided naming convention.

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,11 @@
-#include <Arduino.h>
 #define M_SYSTEM_STATE_IDLE 1
 #define M_SYSTEM_STATE_DISABLED 0
 #define M_SYSTEM_STATE_ERROR 2
 #define M_SYSTEM_STATE_RUNNING 3
+#define M_WATER_LEVEL_THRESHOLD 10
 
 unsigned char g_system_state = M_SYSTEM_STATE_DISABLED;
+unsigned int water_level = 0;
 
 /*
 *
@@ -25,26 +26,37 @@ unsigned char g_system_state = M_SYSTEM_STATE_DISABLED;
 *
 */
 
-volatile unsigned char * o_system_state_LED_port = (unsigned char *) 0x22; 
-volatile unsigned char * o_system_state_LED_ddr = (unsigned char *) 0x21;
+volatile unsigned char * led_porta_data = (unsigned char *) 0x22; 
+volatile unsigned char * led_porta_ddr = (unsigned char *) 0x21;
 
-volatile unsigned char * portd_data = (unsigned char *) 0x2B;
-volatile unsigned char * portd_ddr = (unsigned char *) 0x2A;
-volatile unsigned char * portd_pin = (unsigned char *) 0x29;
+volatile unsigned char * push_button_portd_data = (unsigned char *) 0x2B;
+volatile unsigned char * push_button_portd_ddr = (unsigned char *) 0x2A;
+volatile unsigned char * push_button_portd_pin = (unsigned char *) 0x29;
 volatile unsigned char * push_button_eicra = (unsigned char *) 0x69;
 volatile unsigned char * push_button_eicrb = (unsigned char *) 0x6A;
 volatile unsigned char * push_button_eimsk = (unsigned char *) 0x3D;
 
+volatile unsigned char * water_level_sensor_admux = (unsigned char *) 0x7C;
+volatile unsigned char * water_level_sensor_adcsra = (unsigned char *) 0x7A;
+volatile unsigned char * water_level_sensor_adcsrb = (unsigned char *) 0x7B;
+volatile unsigned int * water_level_sensor_adcl = (unsigned int *) 0x78;
+
 
 void setup() {
 
-    *o_system_state_LED_ddr |= 0b00001111;
+    *led_porta_ddr |= 0b00001111;
     
     *push_button_eicra |= 0b00000001;
     *push_button_eicrb |= 0b00000001;
     *push_button_eimsk |= 0b00000001;
-    *portd_ddr |= 0b00000000;
-    *portd_data |= 0b00000001;
+    *push_button_portd_ddr |= 0b00000000;
+    *push_button_portd_data |= 0b00000001;
+
+    *water_level_sensor_adcsra |= 0b10001000;
+    *water_level_sensor_adcsrb &= 0b00000000;
+    *water_level_sensor_admux |= 0b01000000;
+
+    *water_level_sensor_adcsra |= 0b01000000; 
 
 }
 
@@ -54,23 +66,40 @@ void loop() {
 
         case M_SYSTEM_STATE_IDLE:
 
-            *o_system_state_LED_port &= 0b00000001;
-            *o_system_state_LED_port |= 0b00000001;
+            *led_porta_data &= 0b00000001;
+            *led_porta_data |= 0b00000001;
+            if (water_level < M_WATER_LEVEL_THRESHOLD) {
+              
+                g_system_state = M_SYSTEM_STATE_ERROR;
+              
+            }
 
             break;
 
         case M_SYSTEM_STATE_DISABLED: 
 
-            *o_system_state_LED_port &= 0b00000010;
-            *o_system_state_LED_port |= 0b00000010;
+            *led_porta_data &= 0b00000010;
+            *led_porta_data |= 0b00000010;
 
             break;
 
         case M_SYSTEM_STATE_ERROR:
 
+            *led_porta_data &= 0b00000100;
+            *led_porta_data |= 0b00000100;
+
+            if (water_level >= M_WATER_LEVEL_THRESHOLD) {
+
+              g_system_state = M_SYSTEM_STATE_IDLE;
+              
+            }
+
             break;
 
         case M_SYSTEM_STATE_RUNNING:
+
+            *led_porta_data &= 0b00001000;
+            *led_porta_data |= 0b00001000;
 
             break;
 
@@ -81,17 +110,24 @@ void loop() {
 
 ISR(INT0_vect) {
 
-    if (*portd_pin & 0b00000001) {
+    if (*push_button_portd_pin & 0b00000001) {
       
         for (volatile unsigned int index = 0; index < 200; index++);
         
-        if (*portd_pin & 0b00000001) {
+        if (*push_button_portd_pin & 0b00000001) {
           
             g_system_state = g_system_state ? M_SYSTEM_STATE_DISABLED : M_SYSTEM_STATE_IDLE;
-            while (*portd_pin & 0b00000000);
+            while (*push_button_portd_pin & 0b00000000);
             
         }
         
     }
     
+}
+
+ISR(ADC_vect) {
+
+  water_level = *water_level_sensor_adcl;
+  *water_level_sensor_adcsra |= 0b01000000; 
+  
 }
